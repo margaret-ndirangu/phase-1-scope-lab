63beed66be98cf5fb6e878b2005704b8bdc80268
Margaret <wanjirundirangu43@gmail.com>
2022-06-11 13:38:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+var customerName='bob';
+const leastFavoriteCustomer = 'Arvin'
+function upperCaseCustomerName(){
+  window.customerName= customerName.toUpperCase(); 
+}
+//function setBestCustomer(){
+    //var bestCustomer='not bob';
+    //return window.setBestCustomer=bestCustomer;
+//}
+//window.setBestCustomer= customerName('not bob')
+function setBestCustomer() {
+  window.bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+ window.bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+ 
+  leastFavoriteCustomer = 'Turshley'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
